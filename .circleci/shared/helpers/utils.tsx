f1828556b46e5b0cb5c69e8b5e3ce46bf6ebb25c
Alister Xavier <xavieralister153@gmail.com>
2024-02-18 01:30:39 +0400

    sdadadf

--- .circleci/shared/helpers/utils.tsx
+++
@@ -1,4 +0,0 @@
-export const calculateDiscountedPrice = (price: number, discount: number) => {
-  const final = (price - price * ((discount ?? 0) / 100)).toFixed(2);
-  return final;
-};
--- .circleci/shared/types/requestTypes.ts
+++
@@ -1,9 +0,0 @@
-export type CartUpdate = {
-  id?: string;
-  customerId?: string;
-  products: {
-    id: string;
-    quantity: number;
-  }[];
-  total: number;
-};
